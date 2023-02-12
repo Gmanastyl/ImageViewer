6637cf62dcba81aad49230611a9bf7035449f401
Antyss77 <fragnatiikpvpbest@gmail.com>
2023-02-12 21:49:57 +0100

    Correction of the access path to the images Adding a border around the images Change of color for the buttons
     * ButtonStyle is still not working

--- ImageViewer/MainWindow.axaml.cs
+++ ImageViewer/MainWindow.axaml.cs
@@ -10,11 +10,30 @@ namespace ImageViewer;
 
 public partial class MainWindow : Window {
         private Image _imageControl;
-        private long _currentImageIndex;
-        private string _imageBasePath = $"{AppDomain.CurrentDomain.BaseDirectory}/images/";
+        private int _currentImageIndex;
+        private string[] _imagePaths = new[] {
+            "images/image1.png",
+            "images/image2.png",
+            "images/image3.png",
+            "images/image4.png",
+            "images/image5.png",
+            "images/image6.png",
+            "images/image7.png",
+            "images/image8.png",
+            "images/image9.png",
+            "images/image10.png",
+            "images/image11.png",
+            "images/image12.png",
+            "images/image13.png",
+            "images/image14.png",
+            "images/image15.png",
+            "images/image16.png",
+            "images/image17.png",
+            "images/image18.png",
+        };
         
         private void LogMessage(string message) {
-            using (var streamWriter = new StreamWriter("logfile.txt", true)) {
+            using (var streamWriter = new StreamWriter("logs.txt", true)) {
                 streamWriter.WriteLine(DateTime.Now + ": " + message);
             }
         }
@@ -22,7 +41,7 @@ public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
             _imageControl = this.FindControl<Image>("ImageControl");
-            _currentImageIndex = 1;
+            _currentImageIndex = 0;
             UpdateImage();
             LogMessage("MainWindow initialized");
         }
@@ -33,8 +52,8 @@ public partial class MainWindow : Window {
 
         private void NextImageButton_Click(object sender, RoutedEventArgs e) {
             _currentImageIndex++;
-            if (_currentImageIndex >= Directory.GetFiles(_imageBasePath).Length) {
-                _currentImageIndex = 1;
+            if (_currentImageIndex >= _imagePaths.Length) {
+                _currentImageIndex = 0;
             }
 
             UpdateImage();
@@ -43,13 +62,13 @@ public partial class MainWindow : Window {
         private void PreviousImageButton_Click(object sender, RoutedEventArgs e) {
             _currentImageIndex--;
             if (_currentImageIndex < 0) {
-                _currentImageIndex = new FileInfo(_imageBasePath).Length - 1;
+                _currentImageIndex = _imagePaths.Length - 1;
             }
 
             UpdateImage();
         }
 
         private void UpdateImage() {
-            _imageControl.Source = new Bitmap($"{_imageBasePath}image{_currentImageIndex}.png");
+            _imageControl.Source = new Bitmap(_imagePaths[_currentImageIndex]);
         }
     }
